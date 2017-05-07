326832b6a91a8c4a7b9b52f94d35dfb7b644392b
Hyungjun_An <hyungjun0429@gmail.com>
2017-05-07 20:13:06 +0900

    recursion

--- changed/Lib.hs
+++ changed/Lib.hs
@@ -13,21 +13,31 @@ mkSlaveJob = [| \userFunc -> \them -> do
                             n <- expect
                             send them (userFunc n) |]
 
-simpleMS "master" = [| \host -> \port -> \clos -> \input -> \recProc -> \rtable -> \afterFunc -> do 
-    backend <- initializeBackend host port rtable
-    startMaster backend $
-      (\backend -> \slaves -> do
-          us <- getSelfPid
-          slaveProcesses <- forM slaves $
-            \nid -> spawn nid (clos us)
-          spawnLocal $ forM_ (zip input (cycle slaveProcesses)) $
-            \(m, them) -> send them m
-          res <- recProc (length input)
-          liftIO (afterFunc res)) backend |]
-
+simpleMS "master" = [| 
+          let loop prop input slaveProcesses recProc  = do 
+                        res <- masterJob input slaveProcesses recProc
+                        if prop res
+                          then return res
+                          else loop prop res slaveProcesses recProc
+          in 
+            \host -> \port -> \clos -> \input -> \recProc -> \rtable -> \afterFunc -> \prop -> do 
+              backend <- initializeBackend host port rtable
+              startMaster backend $
+                \slaves -> do
+                us <- getSelfPid
+                slaveProcesses <- forM slaves $
+                  \nid -> spawn nid (clos us)
+                res <- loop prop input slaveProcesses recProc
+                liftIO (afterFunc res) |]
 simpleMS "slave" = [| \host -> \port -> \rtable -> do 
     backend <- initializeBackend host port rtable
     startSlave backend |]
+
+masterJob input slaveProcesses recProc = do
+          spawnLocal $ forM_ (zip input (cycle slaveProcesses)) $
+            \(m, them) -> send them m
+          res <- recProc (length input)
+          return res
 
 mkRecProc "unseq" = [|  let loop 0 xs = return xs
                             loop n xs = do
--- changed/Main.hs
+++ changed/Main.hs
@@ -11,13 +11,22 @@ import Lib
 plus :: Int -> Int
 plus x = x + 1
 
+sqr :: Int -> Int
+sqr x = x ^ 2
+
 input = [1, 2, 3, 4] :: [Int]
 
+prop :: Int -> [Int] -> Bool
+prop n xs | sum xs < n  = True
+          | otherwise   = False
+
 afterFunc :: [Int] -> IO()
-afterFunc xs = putStrLn $ show $ sum xs
+{-afterFunc xs = putStrLn $ show $ sum xs-}
+afterFunc xs = putStrLn $ show xs
 --------------------------------------------------------------------------
 slaveJob :: ProcessId -> Process()
-slaveJob = $(mkSlaveJob) plus
+{-slaveJob = $(mkSlaveJob) plus-}
+slaveJob = $(mkSlaveJob) sqr
 remotable ['slaveJob]
 clos = $(mkClosure 'slaveJob)
 rtable = __remoteTable initRemoteTable
@@ -26,5 +35,5 @@ recProc = $(mkRecProc "unseq")
 main = do
   args <- getArgs
   case args of
-    ["master", host, port] -> $(simpleMS "master") host port clos input recProc rtable afterFunc
+    ["master", host, port] -> $(simpleMS "master") host port clos input recProc rtable afterFunc (prop 100)
     ["slave",  host, port] -> $(simpleMS "slave" ) host port rtable
